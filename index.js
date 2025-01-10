ed532bbb4a0a08377a843eaf0c726264c155e4b7
ElRacoon <lea.granier@outlook.fr>
2025-01-10 12:06:54 +0100

    change port

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(express.json());
 
-app.use("/api/users", utilisateurRoute);
+app.use("/api", utilisateurRoute);
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
