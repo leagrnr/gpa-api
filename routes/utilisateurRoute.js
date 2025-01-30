a880deeeea0dde2456f2fe038df1b4eb26f03410
ElRacoon <lea.granier@outlook.fr>
2025-01-30 16:07:26 +0100

    Middleware

--- routes/utilisateurRoute.js
+++ routes/utilisateurRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const userController = require("../controllers/utilisateurController");
+const authenticateToken = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/users", userController.login);
-router.get("/users/role/:role", userController.getUsersByRole);
-router.get("/users/:id", userController.getUserById);
-router.put("/users/:id", userController.updateUser);
-router.delete("/users/:id", userController.deleteUser);
+router.post("/users", authenticateToken, userController.login);
+router.get("/users/role/:role", authenticateToken, userController.getUsersByRole);
+router.get("/users/:id", authenticateToken, userController.getUserById);
+router.put("/users/:id", authenticateToken, userController.updateUser);
+router.delete("/users/:id", authenticateToken, userController.deleteUser);
 
 module.exports = router;
